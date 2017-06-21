55ad3dbef2fea293f7d44932ea2b2995f114e199
Max Jacobs <Max@Maxs-MacBook-Pro.local>
2017-06-21 13:38:39 -0700

    formatting

--- combine_hashes.rb
+++ combine_hashes.rb
@@ -1,9 +1,3 @@
-# given two hashes, define a method called #combine that merges the two, and if there are duplicate keys, create an array of the associated values
-
-#hash_a = {:a => 1, :b => 2, :c => 3}
-#hash_b = {:a => 2, :d => 4, :e => 5}
-# combine(hash_a, hash_b) #=> {:a => [1,2], :b => 2, :c => 3, :d => 4, :e => 5}
-
 def combine (hash1, hash2)
   
   hash1.each do |k, v|
